df2839f742bd0ee5ada9096717dfc041a4be7ba2
Igor Phelype Guimarães <igor.phelype@gmail.com>
2020-06-18 20:56:02 -0300

    Seed items condicional

--- api/src/database/seeds/create_default_items.ts
+++ api/src/database/seeds/create_default_items.ts
@@ -1,12 +1,17 @@
 import Knex from 'knex'
 
 export async function seed(knex: Knex){
-    await knex('items').insert([
-        {title: 'Lâmpadas', image: 'lampadas.svg'},
-        {title: 'Pilhas e Baterias', image: 'baterias.svg'},
-        {title: 'Papéis e Papelão', image: 'papeis-papelao.svg'},
-        {title: 'Resíduos Eletrônicos', image: 'eletronicos.svg'},
-        {title: 'Resíduos Orgânicos', image: 'organicos.svg'},
-        {title: 'Óleo de Cozinha', image: 'oleo.svg'}
-    ])
+    const items_length = (await knex('items').select()).length
+    if(items_length === 0){
+        await knex('items').insert([
+            {title: 'Lâmpadas', image: 'lampadas.svg'},
+            {title: 'Pilhas e Baterias', image: 'baterias.svg'},
+            {title: 'Papéis e Papelão', image: 'papeis-papelao.svg'},
+            {title: 'Resíduos Eletrônicos', image: 'eletronicos.svg'},
+            {title: 'Resíduos Orgânicos', image: 'organicos.svg'},
+            {title: 'Óleo de Cozinha', image: 'oleo.svg'}
+        ])
+    }else{
+        console.log('Seed cancelada')
+    }
 }
